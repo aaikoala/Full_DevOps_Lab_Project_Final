95b42d9fe6d107e7dfbc70f71274c9c25fc83a53
aaikoala <157149925+aaikoala@users.noreply.github.com>
2025-11-25 14:48:56 +0100

    gitignore

--- src/routes/auto/boom.route.js
+++ src/routes/auto/boom.route.js
@@ -6,7 +6,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/boom", (_req, _res, next) => {
-  const err = new Error("Boom!");
+  const err = new Error("Helloo!");
   err.status = 500;
   next(err);
 });
