0b8ccb8a9bdf2b98bea7200eefdd8432e7ffdc02
YasZl <zeroualyasmine.zy@gmail.com>
2026-01-08 17:08:06 +0100

    Resolve conflicts (SCRUM-18)

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 /**
- * App entrypoint.
+ * App entrypoint
+ * - Connect to MongoDB
+ * - Start the Express server
  */
+
 import app from "./app.js";
-// import { connectToDb } from "./db/mongo.js"; // l'import pour la base de données
+import { connectToDb } from "./db/mongo.js";
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// 2. On utilise le lancement simple (celui que tu avais mis en commentaire)
-app.listen(PORT, () => {
-  console.log(`[server] listening on http://localhost:${PORT}`);
-});
-
-/* la partie connexion DB
 async function start() {
-  try {
-    await connectToDb();
-    app.listen(PORT, () => {
-      console.log(`API running at http://localhost:${PORT}`);
-    });
+  await connectToDb();
 
-  } catch (error) {
-    console.error("Erreur au démarrage du serveur :", error);
-  }
+  app.listen(port, function () {
+    console.log("API running at http://localhost:" + port);
+  });
 }
-start(); 
-*/
+
+start();
